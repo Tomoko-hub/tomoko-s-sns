0aa27ea13fc4a2169d7c1cad5a670616ef313d7f
Tomoko-hub <haisaitomoco@gmail.com>
2022-06-19 14:45:16 +0300

    create register infomation

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const User = require.require("../models/User");
 
+//user registration
+router.post("/register", async (req, res) => {
+    try{
+        const newUser = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        const user = await newUser.save();
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+/*
 router.get("/", (req,res) => {
     res.send("user router");
 });
-
+*/
 module.exports = router;
